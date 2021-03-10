91ff6566ba6f76bc2e047e6cafb01594c5a3a837
Cody Green <cody@codygreen.com>
2021-03-10 16:14:01 -0600

    refactor products

---
+++ api/src/routes/products.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import Products from '../models/products';
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await Products.find();
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(500).json(err)
+    }     
+});
+
+router.get('/:productId', async (req, res) => {
+    try {
+        const product = await Products.find({ id: req.params.productId })
+        res.status(product.length > 0 ? 200 : 404).json(product)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router
--- api/src/server.js
+++ api/src/server.js
@@ -5,6 +5,11 @@ import path from 'path';
 import cors from 'cors';
 import axios from 'axios';
 
+const productsRoutes = require("./routes/products");
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vue-db', {useNewUrlParser: true, useUnifiedTopology: true});
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -25,17 +30,7 @@ const getService = function(serviceName) {
 };
 
 app.use('/images', express.static(path.join(__dirname, '../assets')));
-
-app.get('/api/products', async (req, res) => {
-  const client = await MongoClient.connect(
-    `mongodb://${getService("database").url}:27017`,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-  );
-  const db = client.db('vue-db');
-  const products = await db.collection('products').find({}).toArray();
-  res.status(200).json(products);
-  client.close();
-});
+app.use('/api/products', productsRoutes);
 
 app.get('/api/users/:userId/cart', async (req, res) => {
   const { userId } = req.params;
@@ -52,22 +47,6 @@ app.get('/api/users/:userId/cart', async (req, res) => {
     products.find(product => product.id === id));
   res.status(200).json(cartItems);
   client.close();
-});
-
-app.get('/api/products/:productId', async (req, res) => {
-    const { productId } = req.params;
-    const client = await MongoClient.connect(
-      `mongodb://${getService("database").url}:27017`,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-    );
-    const db = client.db('vue-db');
-    const product = await db.collection('products').findOne({ id: productId });
-    if (product) {
-        res.status(200).json(product);
-    } else {
-        res.status(404).json('Could not find the product!');
-    }
-    client.close();
 });
 
 app.post('/api/users/:userId/cart', async (req, res) => {
