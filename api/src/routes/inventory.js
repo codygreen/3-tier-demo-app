e7c36ce4808639cf2d19587170166b2674c6c564
Cody Green <cody@codygreen.com>
2021-03-10 20:59:46 -0600

    fix inventoryUrl

--- api/src/routes/inventory.js
+++ api/src/routes/inventory.js
@@ -2,12 +2,14 @@ import express from 'express';
 import axios from 'axios';
 const router = express.Router();
 
+const getService = (services, serviceName) => {
+    return services.find(service => service.name === serviceName)
+};
+
 router.get('/', async (req, res) => {
   try {
-    const { data: inventory } = await axios.get(
-    `${req.app.get('inventoryUrl')}/api/inventory`
-    );
-
+    const { url: inventoryUrl} = getService(req.app.get('services'), "inventory");
+    const { data: inventory } = await axios.get(`${inventoryUrl}/api/inventory`);
     res.status(200).json(inventory);
   } catch(error) {
     console.log(error)
